aaf787f957d565289b99dca115192733ab02ae5b
Christian Bookout <christianmbookout@gmail.com>
2022-02-20 13:26:53 -0800

    initialize extension variables in extensions.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,34 @@
+import logging
+import sys
+from extensions import bcrypt, db, mailer
 from flask import Flask, redirect, render_template, request
-from flask_sqlalchemy import SQLAlchemy
+from app.mod_auth import mod_auth
 
 app = Flask(__name__)
+# from app.mod_auth import mod_auth
+app.register_blueprint(mod_auth.login_page)
 
 # Configurations
 app.config.from_object('config')
+
+def init_extensions():
+    """initialize extensions to be used throughout the project"""
+    bcrypt.init_app(app)
+    db.init_app(app)
+    mailer.init_app(app)
+
+def configure_logger(app):
+    """configure the default logging stream"""
+    handler = logging.StreamHandler(sys.stdout)
+    if not app.logger.handlers:
+        app.logger.addHandler(handler)
 
 @app.route('/')
 def index():
     return render_template("index.html")
 
-db = SQLAlchemy(app)
-
 @app.errorhandler(404)
 def not_found(error):
     return render_template('404.html'), 404
+
+init_extensions()
