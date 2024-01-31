df88c0bb922c7547a89d56a975698f2cdd8465f1
ScottKenning1 <skenning128@gmail.com>
2024-01-30 19:06:58 -0800

    First commit part 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,5 @@ import NavBar from "@/components/NavBar";
 import Image from "next/image";
 
 export default function Home() {
-  return (
-    <div className="w-screen min-h-screen">
-      <div className="w-32 h-32 bg-orange-500"></div>
-    </div>
-  );
+  return <div className="w-screen min-h-screen"></div>;
 }
---
+++ src/components/Container.tsx
@@ -0,0 +1,5 @@
+import Image from "next/image";
+
+export default function Container() {
+  return <div className="w-100 "></div>;
+}
