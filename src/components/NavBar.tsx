166c43bc5c5bb21be5dbca7d17aad9eed67d5874
ScottKenning1 <skenning128@gmail.com>
2024-01-30 19:11:31 -0800

    Update nav bar with title

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,7 +1,9 @@
 import Image from "next/image";
 
 export default function NavBar() {
-  return <nav className="w-100 h-16 bg-blue-500">
-    
-  </nav>;
+  return (
+    <nav className="w-100 h-16 bg-blue-500 flex p-8 items-center">
+      <h1 className="text-white text-lg font-bold">Course Resource Hub</h1>
+    </nav>
+  );
 }
